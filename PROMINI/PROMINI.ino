1b8ecfe81f7675eb9ee6864b032280b0b734beca
internessi <internessi@users.noreply.github.com>
2019-07-02 11:54:35 +0200

    Update PROMINI.ino

--- PROMINI/PROMINI.ino
+++ PROMINI/PROMINI.ino
@@ -1,71 +1,45 @@
-/*
-  Blink without Delay
 
-  Turns on and off a light emitting diode (LED) connected to a digital pin,
-  without using the delay() function. This means that other code can run at the
-  same time without being interrupted by the LED code.
 
-  The circuit:
-  - Use the onboard LED.
-  - Note: Most Arduinos have an on-board LED you can control. On the UNO, MEGA
-    and ZERO it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN
-    is set to the correct LED pin independent of which board is used.
-    If you want to know what pin the on-board LED is connected to on your
-    Arduino model, check the Technical Specs of your board at:
-    https://www.arduino.cc/en/Main/Products
 
-  created 2005
-  by David A. Mellis
-  modified 8 Feb 2010
-  by Paul Stoffregen
-  modified 11 Nov 2013
-  by Scott Fitzgerald
-  modified 9 Jan 2017
-  by Arturo Guadalupi
+#define B_LED   A1
+#define G_LED   A2
+#define R_LED   A3
 
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/BlinkWithoutDelay
-*/
-
-// constants won't change. Used here to set a pin number:
-const int ledPin =  LED_BUILTIN;// the number of the LED pin
-
-// Variables will change:
-int ledState = LOW;             // ledState used to set the LED
-
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;        // will store last time LED was updated
-
-// constants won't change:
-const long interval = 1000;           // interval at which to blink (milliseconds)
 
 void setup() {
-  // set the digital pin as output:
-  pinMode(ledPin, OUTPUT);
+
+  Serial.begin(9600);  
+  delay(200);
+  Serial.println("Starting up");
+
+  pinMode       (B_LED,  OUTPUT);  
+  digitalWrite  (B_LED,  HIGH);
+  pinMode       (G_LED,  OUTPUT);  
+  digitalWrite  (G_LED,  HIGH);
+  pinMode       (R_LED,  OUTPUT);  
+  digitalWrite  (R_LED,  HIGH);
+   pinMode(LED_BUILTIN, OUTPUT);
+
 }
 
 void loop() {
-  // here is where you'd put code that needs to be running all the time.
+  
+  delay(200);
+  digitalWrite  (B_LED,  LOW);
+  delay(200);
+  digitalWrite  (B_LED,  HIGH);
+  delay(200);
+  digitalWrite  (G_LED,  LOW);
+  delay(200);
+  digitalWrite  (G_LED,  HIGH);
+  delay(200);
+  digitalWrite  (R_LED,  LOW);
+  delay(200);
+  digitalWrite  (R_LED,  HIGH);
 
-  // check to see if it's time to blink the LED; that is, if the difference
-  // between the current time and last time you blinked the LED is bigger than
-  // the interval at which you want to blink the LED.
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
-
-    // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }
-
-    // set the LED with the ledState of the variable:
-    digitalWrite(ledPin, ledState);
-  }
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000); 
+  
 }
